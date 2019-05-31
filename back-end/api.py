c84fc9ae933393bc60c097dd9a2a402ea6c4821e
anaplb3 <ana.paula@dce.ufpb.br>
2019-05-30 22:09:48 -0300

    bug da paginação consertado e implementação do Flask-RestPlus

--- back-end/api.py
+++ back-end/api.py
@@ -1,53 +1,44 @@
-from flask import Flask, jsonify, render_template, request
-from py2neo import Graph
+from flask import Flask, jsonify, request
 from banco import Dao
 from flask_cors import CORS
-from paginacao import Paginacao
+from flask_restplus import Api, Resource
+
 
 app = Flask(__name__)
+api = Api(app=app)
 
 CORS(app, resources=r"/api/*", headers="Content-Type")
 
-tipo_licitacao = "lic"
-tipo_participante = "part"
-
 dao = Dao()
-pag = Paginacao(dao)
 
+@api.route("/api/licitacao/")
+@api.doc(params={'pagina': 'Pagina que sera acessada'})
+@api.doc(params={'limite': 'Quantos resultados serão retornados'})
+class Licitacao(Resource):
+   def get(self):
+      ''' 
+      Retorna as licitações
+      '''
+      pagina = request.args.get("pagina", 1, int)
+      limite = request.args.get("limite", 20, int)
+      return jsonify(dao.get_licitacoes(pagina, limite))
+
+
+@api.route("/api/participante/")
+@api.doc(params={'pagina': 'Pagina que sera acessada'})
+@api.doc(params={'limite': 'Quantos resultados serão retornados'})
+class Participante(Resource):
+   def get(self):
+      ''' 
+      Retorna os participantes
+      '''
+      pagina = int(request.args.get("pagina", 1, int))
+      limite = int(request.args.get("limite", 20, int))
+      return jsonify(dao.get_participantes(pagina, limite))
+   
 
 @app.route('/api')
 def olar():
     return "inicio"
-
-###########  Rotas para as licitações  ###############
-
-@app.route('/api/licitacoes/', defaults={'page': 1, 'limite': 10})
-@app.route("/api/licitacoes/pagina/<int:page>/limite/<int:limite>", methods=["GET"])
-def show_licitacoes(page, limite):
-   #page = request.args.get("page", 1)
-   #limite = request.args.get("limite", 10)
-   return jsonify(pag.get_licitacoes(page, limite, tipo_licitacao))
-
-###########  Rotas para os participantes  ###############
-
-@app.route('/api/participantes/', defaults={'page': 1, 'limite': 10})
-@app.route("/api/participantes/pagina/<int:page>/limite/<int:limite>", methods=["GET"])
-def show_participantes(page, limite):
-   return jsonify(pag.get_participantes(page, limite, tipo_participante))
-
-###########  Rotas para as buscas  ###############
-
-@app.route("/api/licitacoes/unidadeGestora/<string:unidade>", methods=["GET"])
-def procura_licitacoes_de_unidade_gestora(unidade):
-   return jsonify(dao.get_licitacao_nomeunidadegestora(unidade))
-
-@app.route("/api/licitacoes/ano/<int:ano>", methods=["GET"])
-def procura_licitacoes_por_ano(ano):
-   return jsonify(dao.get_licitacao_por_ano(ano))
-
-@app.route("/api/participante/codigo/<string:codigo>", methods=["GET"])
-def procura_participante_por_codigo(codigo):
-   return jsonify(dao.get_participante_por_codigo(codigo))
-
 
 app.run(debug=True)
--- back-end/banco.py
+++ back-end/banco.py
@@ -3,30 +3,32 @@ from py2neo import Graph
 class Dao:
     def __init__(self):
         self.graph = Graph("bolt://localhost:7687", auth=("neo4j", "tcctcc"))
-        self.num_pag_licitacoes = 0
-        self.num_pag_participantes = 0
 
 
-    def get_licitacoes(self, num_resultados):
-        result = self.graph.run('match (lic:Licitacao) return lic ORDER BY lic.Data SKIP {num_pag} limit {num}',num_pag = self.num_pag_licitacoes, num=num_resultados)
-        self.num_pag_licitacoes += 1
+    def get_licitacoes(self, page, num_resultados):
+        skip = num_resultados * (page - 1)
+        result = self.graph.run('MATCH (lic:Licitacao) RETURN lic ORDER BY lic.Data SKIP {skip} LIMIT {num}',skip = skip, num=num_resultados)
         nodes = [n for n in result]
         return nodes
 
-    def get_participantes(self, num_resultados):
-        result = self.graph.run('match (part:Participante) return part ORDER BY part.NomeParticipante SKIP {num_pag} limit {num}', num_pag=self.num_pag_participantes, num=num_resultados)
-        self.num_pag_participantes += 1
+    def get_participantes(self, page, num_resultados):
+        skip = num_resultados * (page - 1)
+        result = self.graph.run('match (part:Participante) return part ORDER BY part.NomeParticipante SKIP {skip} limit {num}', skip=skip, num=num_resultados)
         nodes = [n for n in result]
         return nodes
 
     def get_licitacao_nomeunidadegestora(self, nome):
+        print("a")
         result = self.graph.run('match (lic:Licitacao{NomeUnidadeGest:$nome}) return lic', nome=nome)
         nodes = [n for n in result]
+        print("tamanho do resultado: ", len(nodes))
         return nodes
 
-    def get_licitacao_por_ano(self, ano):
-        result = self.graph.run('match (lic:Licitacao) WHERE lic.Data CONTAINS {ano} return lic', ano=str(ano))
+    def get_licitacao_por_ano(self, ano, page, limite):
+        skip = limite * (page - 1)
+        result = self.graph.run('MATCH (lic:Licitacao) WHERE lic.Data CONTAINS {ano} RETURN lic ORDER BY lic.Data SKIP {skip} LIMIT {limit}', ano=str(ano), skip=skip, limit=limite)
         nodes = [n for n in result]
+        print("tamanho do resultado: ", len(nodes))
         return nodes
 
     # Busca participante pelo cpf ou cnpj
