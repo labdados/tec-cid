d85b1835301d41d93ad67562599f3d9a3f83cd81
anaplb3 <ana.paula@dce.ufpb.br>
2019-06-04 13:14:41 -0300

    novas rotas

--- back-end/api.py
+++ back-end/api.py
@@ -5,13 +5,13 @@ from flask_restplus import Api, Resource
 
 
 app = Flask(__name__)
-api = Api(app=app)
+api = Api(app=app, doc='/docs')
 
 CORS(app, resources=r"/api/*", headers="Content-Type")
 
 dao = Dao()
 
-@api.route("/api/licitacao/")
+@api.route("/api/licitacao")
 @api.doc(params={'pagina': 'Pagina que sera acessada'})
 @api.doc(params={'limite': 'Quantos resultados serão retornados'})
 class Licitacao(Resource):
@@ -23,8 +23,31 @@ class Licitacao(Resource):
       limite = request.args.get("limite", 20, int)
       return jsonify(dao.get_licitacoes(pagina, limite))
 
+@api.route("/api/licitacao/")
+@api.doc(params={'ano': 'Ano das licitações'})
+@api.doc(params={'pagina': 'Pagina que sera acessada'})
+@api.doc(params={'limite': 'Quantos resultados serão retornados'})
+class Licitacao_por_ano(Resource):
+   def get(self):
+      '''
+      Retorna as licitações de determinado ano
+      '''
+      ano = request.args.get("ano", 2003, int)
+      pagina = request.args.get("pagina", 1, int)
+      limite = request.args.get("limite", 20, int)
+      return jsonify(dao.get_licitacao_por_ano(ano, pagina, limite))
 
-@api.route("/api/participante/")
+@api.route("/api/licitacao/unidade_gestora")
+@api.doc(params={'unidade': 'Unidade gestora'})
+class Licitacao_por_unidade_gestora(Resource):
+   def get(self):
+      '''
+      Retorna as licitações de uma unidade gestora
+      '''
+      unidade = request.args.get('unidade', 'Câmara Municipal de Rio Tinto', str)
+      return jsonify(dao.get_licitacao_nomeunidadegestora(unidade))
+
+@api.route("/api/participante")
 @api.doc(params={'pagina': 'Pagina que sera acessada'})
 @api.doc(params={'limite': 'Quantos resultados serão retornados'})
 class Participante(Resource):
@@ -35,6 +58,17 @@ class Participante(Resource):
       pagina = int(request.args.get("pagina", 1, int))
       limite = int(request.args.get("limite", 20, int))
       return jsonify(dao.get_participantes(pagina, limite))
+
+
+@api.route("/api/participante/codigo")
+@api.doc(params={"cod": "CPF ou CNPJ do participante"})
+class Participante_por_codigo(Resource):
+   def get(self):
+      '''
+      Retorna o participante que tem determinado CPF/CNPJ
+      '''
+      codigo = request.args.get("cod", '35422468000159', str)
+      return jsonify(dao.get_participante_por_codigo(codigo))
    
 
 @app.route('/api')
