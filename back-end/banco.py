f1ab3115949e37d7cd483eea06533904a7905017
Marcus Carvalho <marcuswac@gmail.com>
2019-06-04 12:10:07 -0300

    Criando arquivo de configuracao para o neo4j

--- back-end/banco.py
+++ back-end/banco.py
@@ -1,25 +1,25 @@
 from py2neo import Graph
+import config as cfg 
 
 class Dao:
     def __init__(self):
-        self.graph = Graph("bolt://localhost:7687", auth=("neo4j", "tcctcc"))
-
+        self.graph = Graph(cfg.NEO4J_CFG["host"], auth=(cfg.NEO4J_CFG["user"], cfg.NEO4J_CFG["passwd"]))
 
     def get_licitacoes(self, page, num_resultados):
         skip = num_resultados * (page - 1)
-        result = self.graph.run('MATCH (lic:Licitacao) RETURN lic ORDER BY lic.Data SKIP {skip} LIMIT {num}',skip = skip, num=num_resultados)
+        result = self.graph.run('MATCH (lic:Licitacao) RETURN lic ORDER BY lic.Data SKIP {skip} LIMIT {num}', skip=skip, num=num_resultados)
         nodes = [n for n in result]
         return nodes
 
     def get_participantes(self, page, num_resultados):
         skip = num_resultados * (page - 1)
-        result = self.graph.run('match (part:Participante) return part ORDER BY part.NomeParticipante SKIP {skip} limit {num}', skip=skip, num=num_resultados)
+        result = self.graph.run('MATCH (part:Participante) return part ORDER BY part.NomeParticipante SKIP {skip} limit {num}', skip=skip, num=num_resultados)
         nodes = [n for n in result]
         return nodes
 
     def get_licitacao_nomeunidadegestora(self, nome):
         print("a")
-        result = self.graph.run('match (lic:Licitacao{NomeUnidadeGest:$nome}) return lic', nome=nome)
+        result = self.graph.run('MATCH (lic:Licitacao{NomeUnidadeGest:$nome}) return lic', nome=nome)
         nodes = [n for n in result]
         print("tamanho do resultado: ", len(nodes))
         return nodes
@@ -33,7 +33,7 @@ class Dao:
 
     # Busca participante pelo cpf ou cnpj
     def get_participante_por_codigo(self, codigo):
-        result = self.graph.run("match (part:Participante{ChaveParticipante:$codigo}) return part", codigo=codigo)
+        result = self.graph.run("MATCH (part:Participante{ChaveParticipante:$codigo}) return part", codigo=codigo)
         nodes = [n for n in result]
         return nodes
 
