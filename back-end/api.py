08071186666797aab2cb39732bf3e12c76e056ba
anaplb3 <ana.paula@dce.ufpb.br>
2019-06-11 15:23:45 -0300

    novas rotas adicionadas

--- back-end/api.py
+++ back-end/api.py
@@ -30,8 +30,41 @@ class Licitacao(Resource):
       return jsonify(dao.get_licitacoes(ano, tipoLic, codUni, pagina, limite))
 
 
+@api.route("/api/licitacoes/<string:id>/propostas")
+@api.doc(params={'pagina': 'Página que será acessada'})
+@api.doc(params={'limite': 'Quantos resultados serão retornados'})
+class Propostas(Resource):
+   def get(self, id):
+      '''
+      Retorna as propostas que um participante fez em uma determinada licitação
+      '''
+
+      data = id.split("-")
+      codUnidadeGestora = data[0]
+      codTipoLicitacao = data[1]
+      codLicitacao = data[2]
+
+      pagina = request.args.get("pagina", 1, int)
+      limite = request.args.get("limite", 20, int)
+
+      return jsonify(dao.procurando_propostas(codUnidadeGestora, codTipoLicitacao, codLicitacao, pagina, limite))
+
+
+@api.route("/api/licitacoes/<string:id>")
+class LicitacaoEspecifica(Resource):
+   def get(self, id):
+      '''
+      Retorna uma licitação específica
+      '''
+      data = id.split("-")
+      codUnidadeGestora = data[0]
+      codTipoLicitacao = data[1]
+      codLicitacao = data[2]
+
+      return jsonify(dao.get_licitacao_especifica(codUnidadeGestora, codTipoLicitacao, codLicitacao))
+
+
 @api.route("/api/participantes")
-@api.doc(params={'codPart': 'Código do participante que está sendo buscado'})
 @api.doc(params={'pagina': 'Página que será acessada'})
 @api.doc(params={'limite': 'Quantos resultados serão retornados'})
 class Participante(Resource):
@@ -43,7 +76,15 @@ class Participante(Resource):
       codParticipante = request.args.get("codPart", '', str)
       pagina = request.args.get("pagina", 1, int)
       limite = request.args.get("limite", 20, int)
-      return jsonify(dao.get_participantes(codParticipante, pagina, limite))
+      return jsonify(dao.get_participantes(pagina, limite))
+
+@api.route("/api/participantes/<string:id>")
+class ParticipanteEspecifico(Resource):
+   def get(self, id):
+      '''
+      Retorna um participante específico
+      '''
+      return jsonify(dao.get_participante_por_codigo(id))
 
 
 @app.route('/api')
