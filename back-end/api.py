a06aa9fd4d6cc54541f8fcba5ebf59823f5df04c
kalberroberto <kalber_roberto@hotmail.com>
2019-06-27 20:58:52 +0000

    consertando o swagger da API (docs)

--- back-end/api.py
+++ back-end/api.py
@@ -1,12 +1,30 @@
-from flask import Flask, jsonify, request, Blueprint
+from flask import Flask, jsonify, request, Blueprint, url_for
 from banco import Dao
 from flask_cors import CORS
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, apidoc
+from werkzeug.contrib.fixers import ProxyFix
 
+api_url_prefix = "/tec-cid/api"
 app = Flask(__name__)
+#app.wsgi_app = ProxyFix(app.wsgi_app)
 
-blueprint = Blueprint('api', __name__, url_prefix='/tec-cid/api')
-api = Api(blueprint, doc='/docs')
+class MyCustomApi(Api):
+    def _register_apidoc(self, app: Flask) -> None:
+        conf = app.extensions.setdefault('restplus', {})
+        custom_apidoc = apidoc.Apidoc('restplus_doc', 'flask_restplus.apidoc',
+                                      template_folder='templates', static_folder='static',
+                                      static_url_path=api_url_prefix)
+
+        @custom_apidoc.add_app_template_global
+        def swagger_static(filename: str) -> str:
+            return url_for('restplus_doc.static', filename=filename)
+
+        if not conf.get('apidoc_registered', False):
+            app.register_blueprint(custom_apidoc)
+        conf['apidoc_registered'] = True
+
+blueprint = Blueprint('api', __name__, url_prefix=api_url_prefix)
+api = MyCustomApi(blueprint, doc='/docs')
 app.register_blueprint(blueprint)
 
 CORS(app, resources=r"/tec-cid/api/*", headers="Content-Type")
