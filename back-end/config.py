fda7f20b11defc3ac765f334bab6f66663766b28
kewynakshlley <kewynakshlley@gmail.com>
2019-07-10 18:00:10 -0300

    deletando virtualenv e dependencias desnecessarias

--- back-end/config.py
+++ back-end/config.py
@@ -1,8 +1,10 @@
+import os
+from decouple import config
 NEO4J_CFG = {
-    'host': 'loclhost',
-    'http_port': 7474,
-    'https_port': 7473,
-    'bolt_port': 7687,
-    'user': 'neo4j',
-    'passwd': 'tcctcc'
+    'host': config('HOST_NAME', default='localhost'),
+    'http_port': config('HTTP_PORT', cast=int, default='7474'),
+    'https_port': config('HTTPS_PORT', cast=int, default='7473'),
+    'bolt_port': config('BOLT_PORT', cast=int, default='7687'),
+    'user': config('USER', default='neo4j'),
+    'passwd': config('PASSWORD', default='neo4j')
 }
