4c01eff395f24285d9d4d539f208bd9d7e0f4df6
Marcus Carvalho <marcuswac@gmail.com>
2019-07-12 15:09:09 -0300

    Usando nome de variável padrão para o Neo4j

--- back-end/settings.py
+++ back-end/settings.py
@@ -4,7 +4,7 @@ from decouple import config
 NEO4J_CFG = {
     'host': config('NEO4J_HOST', default='localhost'),
     'port': config('NEO4J_PORT', cast=int, default='7687'),
-    'user': config('NEO4J_USER', default='neo4j'),
+    'user': config('NEO4J_USERNAME', default='neo4j'),
     'passwd': config('NEO4J_PASSWORD', default='neo4j')
 
 }
