b36a6f556c7b7b3f29b3fc8b3130eae5c453f848
Marcus Carvalho <marcuswac@gmail.com>
2019-07-13 19:38:05 -0300

    Fazendo backup dos ultimos arquivos de import

--- database/feed/feed_neo4j.sh
+++ database/feed/feed_neo4j.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # if env variables do not exist, use default values
-NEO4J_DIR_IMPORT=${NEO4J_DIR_IMPORT:-"../import"}
-NEO4J_USERNAME=${NEO4J_USERNAME:-"neo4j"}
-NEO4J_PASSWORD=${NEO4J_PASSWORD:-"neo4j"}
-
-rm $NEO4J_DIR_IMPORT/licitacao.txt
-rm $NEO4J_DIR_IMPORT/participante.txt
+${NEO4J_DIR_IMPORT:="../import"}
+${NEO4J_USERNAME:="neo4j"}
+${NEO4J_PASSWORD:="neo4j"}
 
 curl -o $NEO4J_DIR_IMPORT/licitacao.txt.gz https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Licitacao_Esfera_Municipal.txt.gz
 curl -o $NEO4J_DIR_IMPORT/participante.txt.gz https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Propostas_Licitacao_Esfera_Municipal.txt.gz
+
+mv $NEO4J_DIR_IMPORT/licitacao.txt $NEO4J_DIR_IMPORT/licitacao.txt.bkp
+mv $NEO4J_DIR_IMPORT/participante.txt $NEO4J_DIR_IMPORT/participante.txt.bkp
 
 gunzip $NEO4J_DIR_IMPORT/licitacao.txt.gz
 gunzip $NEO4J_DIR_IMPORT/participante.txt.gz
