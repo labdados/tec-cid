605dd391b3a09a99447e045471e93882ea082937
Marcus Carvalho <marcuswac@gmail.com>
2019-08-16 10:26:33 -0300

    Permitindo ordenação para get licitações

--- back-end/banco.py
+++ back-end/banco.py
@@ -5,9 +5,9 @@ from settings import *
 class Dao:
     def __init__(self):
         self.graph = Graph(host=NEO4J_CFG["host"] , port=NEO4J_CFG["port"],
-                           user=NEO4J_CFG["user"], password=NEO4J_CFG["passwd"]) 
+                           user=NEO4J_CFG["user"], password=NEO4J_CFG["passwd"])
 
-    def get_licitacoes(self, ano, tipo, unidade, pagina, itens):
+    def get_licitacoes(self, ano, tipo, unidade, pagina, itens, ordenar_por, ordem):
         skip = itens * (pagina - 1)
         
         filtros = {}
@@ -22,7 +22,15 @@ class Dao:
         if unidade:
             conditions.append("_.CodUnidadeGest = '{}'".format(unidade))
 
-        result = Licitacao.match(self.graph).where(*conditions).order_by("_.Data").skip(skip).limit(itens)
+        if not ordenar_por:
+            ordenar_por = "_.Data"
+        else:
+            ordenar_por = "_." + ordenar_por
+
+        if ordem.upper() in ["DESC", "ASC"]:
+            ordenar_por += " {}".format(ordem)
+
+        result = Licitacao.match(self.graph).where(*conditions).order_by(ordenar_por).skip(skip).limit(itens)
         nodes = []
         for lic in result:
             node = lic.__node__
@@ -31,7 +39,7 @@ class Dao:
         
         return(nodes)
 
-
+    
     def get_unidades_e_codigos(self):
         result = UnidadeGestora.match(self.graph)
         nodes = [n.__node__ for n in result]
