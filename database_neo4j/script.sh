5cf1192cb53565a296bb7d81c73b084fa355f3a8
Kalber Roberto Pereira Silva <kalber.silva@virtus.ufcg.edu.br>
2019-09-09 16:25:17 -0300

    atualizando so script

--- database_neo4j/script.sh
+++ database_neo4j/script.sh
@@ -33,15 +33,15 @@ mv receita_2016.txt $storage
 mv licitacao.txt $storage
 mv participante.txt $storage
 
-cat cria_index_licitacao.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat cria_index_participante.cypher | cypher-shell -u neo4j -p tcctcc --format plain	
-cat cria_index_unidade_gestora.cypher | cypher-shell -u neo4j -p tcctcc --format plain	
-cat carrega_unidade_gestora.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat carrega_licitacao.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat carrega_participante.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat carrega_partido.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat carrega_candidato.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat carrega_municipio.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat cria_relacionamento_governa.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat cria_relacionamento_lança.cypher | cypher-shell -u neo4j -p tcctcc --format plain
-cat cria_relacionamento_doapara.cypher | cypher-shell -u neo4j -p tcctcc --format plain
+cat cria_index_licitacao.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat cria_index_participante.cypher | cypher-shell -u neo4j -p neo4jpass --format plain	
+cat cria_index_unidade_gestora.cypher | cypher-shell -u neo4j -p neo4jpass --format plain	
+cat carrega_unidade_gestora.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat carrega_licitacao.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat carrega_participante.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat carrega_partido.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat carrega_candidato.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat carrega_municipio.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat cria_relacionamento_governa.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat cria_relacionamento_filiado_a.cypher | cypher-shell -u neo4j -p neo4jpass --format plain
+cat cria_relacionamento_doapara.cypher | cypher-shell -u neo4j -p neo4jpass 
