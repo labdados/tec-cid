dc223f790fd9f0bf319ea6560a4730938424b587
Marcus Carvalho <marcuswac@gmail.com>
2019-10-04 17:56:35 -0300

    Refatorando e adicionando info no docs

--- backend/app/main/service/municipio_service.py
+++ backend/app/main/service/municipio_service.py
@@ -2,31 +2,25 @@ from ..model.municipio import Municipio
 from app.main import db
 from flask import json, jsonify
 
+IMAGE_ATTRIBUTES = ['bandeira', 'brasao']
+
 class MunicipioService:
 
     def get_municipios(self, pagina, limite, campos):
         skip = limite * (pagina - 1)
         
-        args = ''
-        if campos:
-            args = campos.split(",")
-            
-        filtros = []
+        fields = campos.split(",") if campos else ''
         
-        if args:
-            # Formatando os campos de filtro estilo cypher
-            for i in range(0, len(args)): 
-                # Caso seja o último campo é preciso tirar a vírgula
-                if i == len(args) - 1:
-                    filtros.append("m.{} as {} ".format(args[i], args[i]))
-                else:
-                    filtros.append("m.{} as {}, ".format(args[i], args[i]))
+        result = Municipio.match(db).order_by("_.nome").skip(skip).limit(limite)
+        nodes = []
+        for obj in result:
+            node = obj.__node__
+            # remove image attributes
+            for k in IMAGE_ATTRIBUTES:
+                node.pop(k, None)
+            # filter selected fields
+            node = {k: node[k] for k in fields if k in node} if fields else node
+            if node:
+                nodes.append(node)
 
-            res = " ".join(filtros)
-            query = "MATCH (m:Municipio) RETURN {} SKIP {} LIMIT {}".format(res, skip, limite)
-            result = db.run(query).data()
-        else:
-            resultado = Municipio.match(db).order_by("_.nome").skip(skip).limit(limite)
-            result = [n.__node__ for n in resultado]
-        
-        return result
+        return nodes
