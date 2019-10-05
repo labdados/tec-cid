72239bbd4e9e6f27a50d6d7e0f54e8bd8722fd4d
gabriel <gabriel.alves@dce.ufpb.br>
2019-10-05 16:53:07 -0300

    Adequando o frontend as mudanças dos dados

--- frontend/tec-cid/src/app/models/licitacao.model.ts
+++ frontend/tec-cid/src/app/models/licitacao.model.ts
@@ -1,14 +1,18 @@
 export class Licitacao {
     constructor(
-        public CodLicitacao: string,
-        public CodObj: string,
-        public CodTipoLicitacao: string,
-        public CodUnidadeGest: string,
-        public Data: string,
-        public NomeObg: string,
-        public Obs: string,
-        public TipoLicitacao: string,
-        public Valor: string,
+        public ano_homologacao: string,
+        public cd_modalidade: string,
+        public cd_ugestora: string,
+        public data_homologacao: string,        
         public id: string,
+        public modalidade: string,
+        public nome_estagio_processual: string,
+        public nome_setor_atual: string,
+        public numero_licitacao: string,
+        public objeto: string,
+        public situacao_fracassada: string,
+        public url: string,
+        public valor_estimado: string,
+        public valor_licitado: string,
     ) {}
 }
--- frontend/tec-cid/src/app/models/unidade-gestora.model.ts
+++ frontend/tec-cid/src/app/models/unidade-gestora.model.ts
@@ -1,6 +1,11 @@
 export class UnidadeGestora {
     constructor(
-        public CodUnidadeGest:string,
-        public NomeUnidadeGest:string
+        public cd_ugestora:string,
+        public jurisdicionado_id:string,
+        public municipio:string,
+        public nome:string,
+        public nome_esfera_jurisdicionado:string,
+        public nome_tipo_administracao_jurisdicionado:string,
+        public nome_tipo_jurisdicionado:string,
     ){}
 }
