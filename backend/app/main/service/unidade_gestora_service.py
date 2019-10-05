4e85560f129050213c799066d3d2dbc2a3db9507
anaplb3 <ana.paula@dcx.ufpb.br>
2019-10-05 10:07:46 -0300

    agora existe a busca das unidades gestoras de determinado município

--- backend/app/main/service/unidade_gestora_service.py
+++ backend/app/main/service/unidade_gestora_service.py
@@ -3,7 +3,12 @@ from app.main import db
 
 class UnidadeGestoraService:
 
-	def get_unidades_gestoras(self):
-            result = UnidadeGestora.match(db).where("_.nome_esfera_jurisdicionado = 'Municipal'")
+	def get_unidades_gestoras(self, nome_municipio):
+            result = ''
+            if nome_municipio:
+                result = UnidadeGestora.match(db).where("_.municipio = '{}' AND _.nome_esfera_jurisdicionado = 'Municipal'".format(nome_municipio.upper()))
+            else:
+                result = UnidadeGestora.match(db).where("_.nome_esfera_jurisdicionado = 'Municipal'")
+            
             nodes = [n.__node__ for n in result]
             return nodes
