106c85f20201eba841dd6a6b8117b7d38e766571
anaplb3 <ana.paula@dcx.ufpb.br>
2019-10-19 21:50:40 -0300

    criado endpoint do candidato especifico e ajustado o id do candidato no get_gestoes

--- backend/app/main/controller/candidato_controller.py
+++ backend/app/main/controller/candidato_controller.py
@@ -2,7 +2,7 @@ from ..service.candidato_service import CandidatoService
 from flask_restplus import Resource, Namespace
 from flask import request, jsonify
 
-candidato = CandidatoService()
+candidato_service = CandidatoService()
 
 api = Namespace('Candidato', 'Operações relacionadas aos candidatos de partidos políticos')
 
@@ -16,5 +16,15 @@ class CandidatoList(Resource):
       '''
       pagina = request.args.get("pagina", 1, int)
       limite = request.args.get("limite", 20, int)
-      candidatos = candidato.get_candidatos(pagina, limite)
+      candidatos = candidato_service.get_candidatos(pagina, limite)
       return jsonify({"dados": candidatos})
+   
+@api.route("/<string:id>")
+@api.doc(params={"id": "ID do candidato"})
+class Candidato(Resource):
+       def get(self, id):
+         '''
+         Retorna um candidato específico 
+         '''
+         candidato = candidato_service.get_candidato_por_id(id)
+         return jsonify({"dados": candidato})
