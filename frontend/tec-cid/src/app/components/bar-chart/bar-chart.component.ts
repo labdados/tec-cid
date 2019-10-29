b7f9833767416abbceca7cb4dd1aaa854a165150
mirianbatista <mirian.batista@dce.ufpb.br>
2019-10-29 12:46:07 -0300

    gráfico com erros

--- frontend/tec-cid/src/app/components/bar-chart/bar-chart.component.ts
+++ frontend/tec-cid/src/app/components/bar-chart/bar-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, Input, OnChanges, ViewChild, ViewEncapsulation, HostListener } from '@angular/core';
 import * as d3 from 'd3';
-import { DataModel } from 'src/app/models/data.model';
+//import { DataModel } from 'src/app/models/data.model';
 import { EstatisticasService } from 'src/app/services/estatisticas.service';
 
 @Component({
@@ -20,7 +20,7 @@ export class BarChartComponent implements OnChanges {
   @Input()
   data: any[];
 
-  margin = {top: 20, right: 20, bottom: 30, left: 40};
+  margin = {top: 0, right: 0, bottom: 90, left: 138};
 
   constructor(
     private estatisticasService: EstatisticasService
@@ -60,44 +60,44 @@ export class BarChartComponent implements OnChanges {
         .attr('height', element.offsetHeight);
 
     const contentWidth = element.offsetWidth - this.margin.left - this.margin.right;
-    const contentHeight = element.offsetHeight - this.margin.top - this.margin.bottom;
+    const contentHeight = element.offsetHeight + this.margin.top + this.margin.bottom;
 
     const x = d3
-      .scaleBand()
-      .rangeRound([0, contentWidth])
-      .padding(0.1)
-      .domain(data.map(d => d.nome_municipio));
-
-    const y = d3
       .scaleLinear()
       .rangeRound([contentHeight, 0])
       .domain([0, d3.max(data, d => Number(d.valor_licitacoes))]);
+
+    const y = d3
+    .scaleBand()
+    .range([0, contentHeight])
+    .padding(0.22)
+    .domain(data.map(d => d.nome_municipio));
 
     const g = svg.append('g')
       .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
     g.append('g')
+      .style("font", "12px sans-serif")
       .attr('class', 'axis axis--x')
       .attr('transform', 'translate(0,' + contentHeight + ')')
       .call(d3.axisBottom(x));
 
     g.append('g')
+        .style("font", "14px sans-serif")
       .attr('class', 'axis axis--y')
       .call(d3.axisLeft(y).ticks(10))
       .append('text')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 6)
-        .attr('dy', '0.71em')
-        .attr('text-anchor', 'end')
-        .text('Frequency');
+      .attr('y', 6)
+        .attr('dy', '4.91em')
+        .attr('text-anchor', 'end');
 
     g.selectAll('.bar')
       .data(data)
       .enter().append('rect')
         .attr('class', 'bar')
-        .attr('x', d => x(d.nome_municipio))
-        .attr('y', d => y(d.valor_licitacoes))
-        .attr('width', x.bandwidth())
-        .attr('height', d => contentHeight - y(d.valor_licitacoes));
+        .attr('y', d => y(d.nome_municipio))
+        .attr('x', d => x(d.valor_licitacoes))
+        .attr('height', y.bandwidth())
+        .attr('width', d => contentWidth - x(d.valor_licitacoes));
   }
 }
