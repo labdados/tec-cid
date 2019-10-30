a296ce5f82f0f6b0bc4e16f89bf02f8b7f1bcb4a
gabriel <gabriel.alves@dce.ufpb.br>
2019-10-29 21:30:59 -0300

    Novo visual do sistema

--- frontend/tec-cid/src/app/services/municipios.service.ts
+++ frontend/tec-cid/src/app/services/municipios.service.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { UnidadeGestora } from '../models/unidade-gestora.model';
 import { API_URL } from './tc.api';
 import { Municipio } from '../models/municipio.model';
-import { identifierModuleUrl } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MunicipiosService {
 
-  // municipios: UnidadeGestora [];
-  // unidadesGestoras: UnidadeGestora [] = [];
   municipios: Municipio[];
   municipio: Municipio = new Municipio('', '', '', '', '', '', '', '', '', '', '', '', '')
 
@@ -20,44 +16,42 @@ export class MunicipiosService {
     private http: HttpClient
   ) { }
 
-  getMunicipios(){
-    return this.http.get<any>(`${API_URL}/municipios`, 
-    {
-      params: 
+  getMunicipios() {
+    return this.http.get<any>(`${API_URL}/municipios`,
       {
-        atributos: 'id,nome',
-        pagina: '1',
-        limite: '230'
-      }
-    }).subscribe(data => {
-      this.municipios = data.dados;
-    })
+        params:
+        {
+          atributos: 'id,nome',
+          pagina: '1',
+          limite: '230'
+        }
+      }).subscribe(res => { this.municipios = res.dados })
   }
 
-  getLicitacoesMunicipio(idMunicipio:any, pagina:number){
-    return this.http.get<any>(`${API_URL}/licitacoes`, 
-    {
-      params: {
-        dataInicio: '2017-01-01',
-        dataFim: '2020-12-31',
-        limite: "9999",
-        pagina: `${pagina}`,
-        ordenarPor: 'data_homologacao',
-        ordem: 'DESC',
-        idMunicipio: `${idMunicipio}`
-      }
-    })    
+  getLicitacoesMunicipio(idMunicipio: any, pagina: number) {
+    return this.http.get<any>(`${API_URL}/licitacoes`,
+      {
+        params: {
+          dataInicio: '2017-01-01',
+          dataFim: '2020-12-31',
+          limite: "9999",
+          pagina: `${pagina}`,
+          ordenarPor: 'data_homologacao',
+          ordem: 'DESC',
+          idMunicipio: `${idMunicipio}`
+        }
+      })
 
     // return this.http.get<any>(`${API_URL}/licitacoes?limite=10&pagina=${pagina}&ano=${ano}&codUni=${codUni}&tipoLic=${tipoLic}`)    
   }
 
-  getMunicipio(id:string) {
+  getMunicipio(id: string) {
     return this.http.get<any>(`${API_URL}/municipios/${id}`).subscribe(res => {
       this.municipio = res.dados[0];
     })
   }
 
-  getGestao(id: string, ano:number) {
+  getGestao(id: string, ano: number) {
     return this.http.get<any>(`${API_URL}/municipios/${id}/gestoes`, {
       params: {
         ano: `${ano}`
