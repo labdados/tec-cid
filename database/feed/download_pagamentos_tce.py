e13826325cbbcd2b13657d4490c64cb67053362d
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2019-11-19 15:35:30 -0300

    Junção dos downloads de Empenhos, Pagtos e Licitações

--- database/feed/download_pagamentos_tce.py
+++
@@ -1,7 +0,0 @@
-from etl_utils import download_file
-
-PAGAMENTOS_URL = 'https://dados.tce.pb.gov.br/TCE-PB-SAGRES-Pagamentos_Esfera_Municipal.txt.gz'
-OUTPUT_FILE = '../../dados/TCE-PB-SAGRES-Pagamentos_Esfera_Municipal.txt.gz'
-
-if __name__ == '__main__':
-    download_file(PAGAMENTOS_URL, OUTPUT_FILE)
