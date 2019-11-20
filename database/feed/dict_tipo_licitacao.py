cd580644d5ccce2c2225e069d73987d721f38ee7
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2019-11-20 20:43:38 -0300

    Criação dos métodos para manipulação do dicionário CD_TIPOS_LICITACOES

--- database/feed/dict_tipo_licitacao.py
+++ database/feed/dict_tipo_licitacao.py
@@ -1,21 +1,26 @@
 import csv
+import operator
 
 CSV_TIPOS_LICITACOES = '../../dados/tipo_licitacao.csv'
 TIPO_LICITACAO_IDX = 0
 CD_TIPO_LICITACAO_IDX = 1
 
-
-def get_dictionary(csv_file):
-    with open(csv_file, 'r') as file:
+def get_dictionary(csv_tipos_licitacoes):
+    with open(csv_tipos_licitacoes, 'r') as file:
         csv_data = csv.reader(file, delimiter=",")
-
-        # Skip title line
-        next(csv_data)
+        next(csv_data) # Skip title line
         dictionary = {}
         for row in csv_data:
-            dictionary[row[TIPO_LICITACAO_IDX]] = row[CD_TIPO_LICITACAO_IDX]
+            dictionary[row[TIPO_LICITACAO_IDX]] = int(row[CD_TIPO_LICITACAO_IDX])
     return dictionary
 
+def update_csv(csv_tipos_licitacoes, aux_dicionary):
+    with open(csv_tipos_licitacoes, 'a') as file:
+        csv_data = csv.writer(file, delimiter=",")
 
-res = get_dictionary(CSV_TIPOS_LICITACOES)
-print(res)
+        for key in aux_dicionary:
+            csv_data.writerow([key, int(aux_dicionary[key])])
+        
+def get_max_codigo_licitacao(dictionary):
+    result = max(dictionary.items(), key=operator.itemgetter(1))[CD_TIPO_LICITACAO_IDX]
+    return result
