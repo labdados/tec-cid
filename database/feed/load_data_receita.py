8c60c6a0c73a70e91160fe356eccc0815aed2cdf
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-02-02 16:27:15 -0300

    Adiçao do carregamento dos dados da Receita Federal

---
+++ database/feed/load_data_receita.py
@@ -0,0 +1,17 @@
+import sys
+from decouple import config
+from etl_utils import query_from_file
+from py2neo import Graph
+
+if __name__ == '__main__':
+    user = sys.argv[1] if len(sys.argv) > 1 else config('NEO4J_USER', default='neo4j')
+    password = sys.argv[2] if len(sys.argv) > 2 else config('NEO4J_PASSWORD', default='password')
+    
+    neo4j = Graph("localhost", user=user, password=password)
+    cypher_files = [
+        'cria_index_empresa_cnpj.cypher',
+        'cria_rel_empresa_foi_participante.cypher'
+    ]
+    
+    for cypher_file in cypher_files:
+        query_from_file(neo4j, cypher_file)
