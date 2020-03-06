7091921133f3312f315886caad72b58897f3b0e3
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-03-05 23:30:44 -0300

    Adiçao do download dos dados de socio

--- database/feed/download_data_receita.py
+++ database/feed/download_data_receita.py
@@ -1,7 +1,11 @@
 import gdown
 
+SOCIOS_URL = 'https://drive.google.com/uc?id=19DeL9HbOebNXRFZ_bmmZu1uAogdY8ZGI'
+OUTPUT_FILE_SOCI0S = '../../dados/socio.csv.gz'
+
 EMPRESAS_URL = 'https://drive.google.com/uc?id=1c_gwLqaVQzRmY2IYCMyPDDCoC8OtTuiW'
 OUTPUT_FILE_EMPRESAS = '../../dados/empresa.csv.gz'
 
 
 gdown.download(EMPRESAS_URL, output=OUTPUT_FILE_EMPRESAS)
+gdown.download(SOCIOS_URL, output=OUTPUT_FILE_SOCI0S)
