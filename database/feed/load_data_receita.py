96ba65766e435e2bf94024f27cfc6bd8979c3e60
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-03-05 23:31:27 -0300

    Adição dos arquivos cypher de sócios no load.py

--- database/feed/load_data_receita.py
+++ database/feed/load_data_receita.py
@@ -10,7 +10,9 @@ if __name__ == '__main__':
     neo4j = Graph("localhost", user=user, password=password)
     cypher_files = [
         'cria_index_empresa_cnpj.cypher',
-        'cria_rel_empresa_foi_participante.cypher'
+        'cria_index_socio.cypher',
+        'cria_rel_empresa_foi_participante.cypher',
+        'cria_rel_empresa_tem_socio.cypher'
     ]
     
     for cypher_file in cypher_files:
