20d272111be934dcb3ecaedf3737e1063abfa48c
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-04-23 12:28:46 -0300

    Adição dos testes de participante de empenhos

--- database/test/run.py
+++ database/test/run.py
@@ -1,13 +1,31 @@
 import os
+import datetime
 
 PREFIX = "python"
 TEST_FILES = [
     'test_candidato.py',
     'test_unidade_gestora.py',
     'test_socio.py',
+    'test_participante.py',
     'test_empresa.py'
 ]
 
+def get_time():
+    now = datetime.datetime.now()
+    hours = now.hour
+    minutes = now.minute
+    seconds = now.second
+
+    return '{}h:{}m:{}s'.format(hours, minutes, seconds)
+
+
 if __name__ == "__main__":
+    start_time = '[START TIME]: ' + get_time()
+
     for file in TEST_FILES:
         os.system(PREFIX + " " + file)
+    
+    finish_time = '[FINISH TIME]: ' + get_time()
+
+    print(start_time)
+    print(finish_time)
