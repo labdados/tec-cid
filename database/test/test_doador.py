ca023cbac9caf39496d6b476cf1e0618639dd273
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-05-01 22:41:43 -0300

    Novo teste unitario para doadores sem candidatos associados

--- database/test/test_doador.py
+++ database/test/test_doador.py
@@ -1,12 +1,14 @@
-import sys
-import os
-from decouple import config
-from py2neo import Graph
+import unittest
 
-from neo4j_utils import *
+from neo4j_utils import Neo4jUtils
+from assert_utils import AssertUtils
+from test_candidato import TestCandidato
 
 neo4j_utils = Neo4jUtils()
-class TestDoador():
+assert_utils = AssertUtils()
+test_candidatos = TestCandidato()
+
+class TestDoador(unittest.TestCase):
 
     COUNT_INDEX = 0
 
@@ -16,3 +18,28 @@ class TestDoador():
         total_doadores = result[self.COUNT_INDEX]['total']
 
         return total_doadores != 0
+    
+    def assert_total_doadores_e_candidatos(self):
+        total_candidatos = test_candidatos.comparar_total_candidatos()
+        total_doadores = self.comparar_total_doadores()
+
+        if (not total_candidatos):
+            raise unittest.SkipTest("O total de candidatos de 2016 não é 11639.")
+
+        if (not total_doadores):
+            raise unittest.SkipTest("O total de doadores não pode ser zero!")
+
+
+    def test_doadores_que_nao_doaram_para_candidatos(self):
+        self.assert_total_doadores_e_candidatos()
+
+        query = "MATCH (d:Doador) WHERE NOT (:Doador)-[:DOOU_PARA]-(:Candidato) RETURN COUNT(d) AS total;"
+        result = neo4j_utils.get_query_response(query)
+        total = result[self.COUNT_INDEX]['total']
+
+        self.assertTrue(total == 0)
+
+
+if __name__ == "__main__":
+    test = unittest.TestLoader().loadTestsFromTestCase(TestDoador)
+    unittest.TextTestRunner(verbosity=2).run(test)
