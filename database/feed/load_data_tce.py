5bd16b67764633702c5d041bf06f1bb8ba057dd9
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-05-05 12:56:11 -0300

    Refatorando nomes de arquivos cypher em database/feed

--- database/feed/load_data_tce.py
+++ database/feed/load_data_tce.py
@@ -6,6 +6,8 @@ from py2neo import Graph
 import traceback
 import datetime
 
+PREFIX = 'cypher/'
+
 def get_time():
     now = datetime.datetime.now()
     hours = now.hour
@@ -22,35 +24,35 @@ if __name__ == '__main__':
     
     neo4j = Graph("localhost", user=user, password=password)
     cypher_files = [
-        'cria_index_unidade_gestora.cypher',
-        'cria_index_municipio.cypher',
-        'cria_index_municipio_nome.cypher',
-        'cria_index_licitacao.cypher',
-        'cria_index_participante.cypher',
-        'cria_index_empenho.cypher',
-        'cria_index_empenho_uorcamentaria.cypher',
+        'index_unidade_gestora.cypher',
+        'index_municipio.cypher',
+        'index_municipio_nome.cypher',
+        'index_licitacao.cypher',
+        'index_participante.cypher',
+        'index_empenho.cypher',
+        'index_empenho_uorcamentaria.cypher',
 
-        #'cria_index_pagamento_id_empenho.cypher',
-        #'cria_index_pagamento_valor.cypher',
-        #'cria_index_pagamento_data.cypher',
-        #'cria_index_pagamento_ugestora.cypher',
+        #'index_pagamento_id_empenho.cypher',
+        #'index_pagamento_valor.cypher',
+        #'index_pagamento_data.cypher',
+        #'index_pagamento_ugestora.cypher',
 
-        'carrega_licitacoes_propostas.cypher',
-        'carrega_municipios.cypher',
-        'carrega_nodes_empenhos.cypher',
-        'carrega_nodes_participantes.cypher',
-        'cria_rel_gerou_empenho.cypher',
-        'cria_rel_empenhado_para.cypher'
+        'rel_licitacoes_propostas.cypher',
+        'nodes_municipios.cypher',
+        'nodes_empenhos.cypher',
+        'nodes_participantes.cypher',
+        'rel_gerou_empenho.cypher',
+        'rel_empenhado_para.cypher'
 
         #'deleta_rel_gerou_pagamento.cypher',
         #'deleta_pagamentos.cypher',
-        #'carrega_nodes_pagamentos.cypher',
-        #'cria_rel_gerou_pagamento.cypher'
+        #'nodes_pagamentos.cypher',
+        #'rel_gerou_pagamento.cypher'
     ]
 
     try:
         for cypher_file in cypher_files:
-            query_from_file(neo4j, cypher_file)
+            query_from_file(neo4j, PREFIX + cypher_file)
 
     except Exception as exception:
         print(exception)
