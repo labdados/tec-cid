9b4136480ce46ed11eb10d228b817efa88916f5c
Jhonatas Flor de Sousa <58887452+FlorSousa@users.noreply.github.com>
2021-02-20 22:47:02 -0300

    Changes on 'Pesquisa' component
    Adding async functions that wait response for server, a moment to load card and a label of warn error

--- frontend/tec-cid/src/app/pages/pesquisa/pesquisa.component.ts
+++ frontend/tec-cid/src/app/pages/pesquisa/pesquisa.component.ts
@@ -1,29 +1,72 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 //import { DataModel } from 'src/app/models/data.model';
 import { EstatisticasService } from 'src/app/services/estatisticas.service';
-
+import {BarchartEmpresasComponent} from 'src/app/components/barchart-empresas/barchart-empresas.component'
 @Component({
   selector: 'app-pesquisa',
   templateUrl: './pesquisa.component.html',
   styleUrls: ['./pesquisa.component.css']
 })
 export class PesquisaComponent implements OnInit {
-
   data: Observable<any>;
   dataEmpresas: Observable<any>;
+  ExibirErrorEmpresa: boolean = false;
+  ExibirErrorMunicipio: boolean = false;
+  Loaded: boolean = true;
+  constructor(
+    private estatisticasService: EstatisticasService
+  ){}
 
-  constructor(private http: HttpClient, private estatisticasService: EstatisticasService) {
-    this.estatisticasService.getRankingMunicipios(10).subscribe(res => {
-      this.data = res.dados;
+
+  ExibeErrorTopEmpresasData(){
+    this.ExibirErrorEmpresa = true
+    }
+
+  ExibeErrorMunicipiosData(){
+    this.ExibirErrorMunicipio = true
+    }
+
+  async TopMunicipios(){
+    const RetornoMunicipios =new Promise(resolve=>{
+        this.estatisticasService.getRankingMunicipios(10).subscribe(res =>{
+          if(res.dados.length > 0){
+            this.Loaded = false
+            return this.data = res.dados;
+          }
+          else{
+            return this.ExibeErrorMunicipiosData()
+          }
+        })
     })
-    this.estatisticasService.getRankingEmpresas(10).subscribe(res => {
-      this.dataEmpresas = res.dados;
-    })
+    return RetornoMunicipios
   }
+  
+
+
+ async TopEmpresas(){
+    const RetornoEmpresas = new Promise(resolve=>{
+        this.estatisticasService.getRankingEmpresas(10).subscribe(async res =>{
+          if(res.dados.length > 0){ 
+            this.dataEmpresas = res.dados
+             
+          }
+          else{
+            return this.ExibeErrorTopEmpresasData()
+          }
+        })
+    })
+    return RetornoEmpresas
+  }
+  
+ async InsertData(){
+    await Promise.all([this.TopMunicipios(),this.TopEmpresas()]);
+
+}
 
   ngOnInit() {
+    this.InsertData()
   }
 
 }
+
