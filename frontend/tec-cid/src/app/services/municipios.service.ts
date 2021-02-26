f850b8c45648953156dffab6610ee34eeb9299d5
Jhonatas Flor de Sousa <58887452+FlorSousa@users.noreply.github.com>
2021-02-26 15:56:52 -0300

    Arrumando link
    Desfazendo erro no link do metodo teste

--- frontend/tec-cid/src/app/services/municipios.service.ts
+++ frontend/tec-cid/src/app/services/municipios.service.ts
@@ -17,7 +17,7 @@ export class MunicipiosService {
   ) { }
 
   public teste():Observable<any>{
-    return this.http.get<any>(`${API_URL/municipios',{
+    return this.http.get<any>(`${API_URL}/municipios`,{
         params:{
           atributos: 'id,nome',
           pagina: '1',
