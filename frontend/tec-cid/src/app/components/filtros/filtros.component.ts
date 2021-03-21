41c6b571b617a9043bddee21fa42a842ec46bb53
Jhonatas Flor de Sousa <58887452+FlorSousa@users.noreply.github.com>
2021-03-20 21:31:14 -0300

    Mudanças importantes para o data binding
    Aqui eu fiz algumas alterações como adicionar um hook NgOnChanges que muda o valor do Menu para nome da cidade e fiz uma ligação entre NomeCidade com get Municipio

--- frontend/tec-cid/src/app/components/filtros/filtros.component.ts
+++ frontend/tec-cid/src/app/components/filtros/filtros.component.ts
@@ -1,24 +1,26 @@
-import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
-import { MunicipiosService } from '../../services/municipios.service';
-import { Municipio } from '../../models/municipio.model';
-import { UnidadeGestoraService } from '../../services/unidade-gestora.service';
-import { EstatisticasService } from '../../services/estatisticas.service';
+import { Component, OnInit, ElementRef, AfterViewInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { MunicipiosService } from 'src/app/services/municipios.service';
+import { Municipio } from 'src/app/models/municipio.model';
+import { UnidadeGestoraService } from 'src/app/services/unidade-gestora.service';
+import { EstatisticasService } from 'src/app/services/estatisticas.service';
 import { Router } from '@angular/router';
-import { Gestao } from '../../models/gestao.model';
-import {CandidatosService} from '../../services/candidatos.service'
+import { Gestao } from 'src/app/models/gestao.model';
+import {CandidatosService} from 'src/app/services/candidatos.service';
+import {PesquisaComponent} from 'src/app/pages/pesquisa/pesquisa.component';
 @Component({
   selector: 'app-filtros',
   templateUrl: './filtros.component.html',
   styleUrls: ['./filtros.component.css']
 })
-export class FiltrosComponent implements OnInit, AfterViewInit {
+export class FiltrosComponent implements OnInit,OnChanges, AfterViewInit {
   Loader : boolean = false
   exibir: boolean = false;
+  //instancia cidade usando a classe Municipio
   cidade: Municipio;
   valorLicitacoes: any;
   gestao: Gestao;
   ErroLoad: boolean = false
-
+  @Input() NomeCidade:String = " "
   configCidades = {
     displayKey: "nome",
     search: true,
@@ -35,12 +37,24 @@ export class FiltrosComponent implements OnInit, AfterViewInit {
     private estatisticasService: EstatisticasService,
     private candidatosService: CandidatosService,
     private router: Router,
-    private elementRef: ElementRef
+    private elementRef: ElementRef,
+    private Pesquisa: PesquisaComponent
   ) { }
-
   ngOnInit() {
-    this.municipiosService.getMunicipios();
+    this.RecebeMunicipios()
   }
+
+  ngOnChanges(changes: SimpleChanges){
+      if(changes.NomeCidade.currentValue !==""){
+        this.Pesquisa.MudaMunicipioBTN()
+      }
+  }
+  RecebeMunicipios(){
+    this.municipiosService.ReqMunicipios().subscribe(data=>{
+      this.municipiosService.municipios = data.dados
+    })
+  }
+
 
   ngAfterViewInit() {
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#F5F5F5';
@@ -50,9 +64,11 @@ export class FiltrosComponent implements OnInit, AfterViewInit {
     return this.municipiosService.municipios
   }
 
+  /*
   get municipio() {
     return this.municipiosService.municipio
   }
+  */
 
   get unidadesGestoras() {
     return this.unidadeGestoraService.unidadesGestoras
@@ -61,17 +77,24 @@ export class FiltrosComponent implements OnInit, AfterViewInit {
   get candidato() {
     return this.candidatosService.candidato
   }
-
+  /*
   displayFn(municipio?: Municipio): string | undefined {
     return municipio ? municipio.nome : undefined;
   }
+*/
 
   filtroMunicipio() {
-    this.municipiosService.getMunicipio(this.cidade.id);
-    this.getValorLicitacoes(this.cidade.id);
-    this.ErroLoad = false
-    this.GerenciaLoadBusca()
-    this.unidadeGestoraService.getUnidadesGestorasByMunicipio(this.cidade.nome);
+    if(this.cidade.id == undefined){
+      //evita erro de selecionar algo que não existe
+    }
+    else{
+      this.municipiosService.getMunicipio(this.cidade.id);
+      this.getValorLicitacoes(this.cidade.id);
+      this.ErroLoad = false
+      this.GerenciaLoadBusca()
+      this.Pesquisa.reset()
+      this.unidadeGestoraService.getUnidadesGestorasByMunicipio(this.cidade.nome);
+    }
   }
 
   getValorLicitacoes(id: string) {
@@ -86,8 +109,12 @@ export class FiltrosComponent implements OnInit, AfterViewInit {
     const Espera = new Promise(resolve=>{
       this.municipiosService.getGestao(this.cidade.id,ano).subscribe(async res => {
           if(res.dados.length > 0){
+            this.Pesquisa.barchartVisivel = false
+            this.Pesquisa.Loaded = false
+            this.Pesquisa.MenuVisivel = true
             this.Loader = false
             this.exibir = true
+            this.Pesquisa.ShowTabela = true
             this.gestao = res.dados[0]
             return this.candidatosService.getCandidato(this.gestao.id_candidato);
           }
@@ -103,11 +130,6 @@ export class FiltrosComponent implements OnInit, AfterViewInit {
       this.Loader = true
       this.exibir = false
       const retorno = await this.getGestao()
-  }
-
-
-  exibirMunicipio() {
-    this.router.navigate(['/municipio', this.cidade.id])
   }
 
   ExibirErro(){
